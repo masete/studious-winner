7c4abe55cfe6d3a415c3e4bd57c36faf6f1f0d95
masete <nicholasmasete72@gmail.com>
2022-07-08 17:05:02 +0300

    added coponent skeletons

--- src/components/Tours/index.js
+++ src/components/Tours/index.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './index.css'
 
+const url ='https://course-api.netlify.app/api/react-tours-project';
 
 const Tours = () =>{
+    const [loading, setLoading] = useState(true)
+    const [tours, setTours] = useState([])
+
+    if(loading){
+        return(
+        <>
+            <div>
+                <loading />
+            </div>
+        </>)
+    }
     return (
         <>
             <div className='frist-div'>
-                <h1>lets tour</h1>
+                <h1><Tours /></h1>
             </div>
         </>
     )
